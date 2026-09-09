78ece754c7cd5fe6928ec25ec12068793fd44a42
Robyn Wright <Robyn.wright@dal.ca>
2026-09-08 22:26:51 -0300

    Build after module 4

--- _site/assets/js/better404/better404.js
+++ _site/assets/js/better404/better404.js
@@ -2,11 +2,11 @@
 // Get root
 
 // Vars
-let homeUrl = "http://localhost:4000/";
+let homeUrl = "https://microbiomehelper.ca/";
 // with no trailing '/'
 homeUrl = homeUrl.replace(/\/+$/, "");
 let currentUrl = window.location.href;
-let sitemap = "http://localhost:4000/sitemap.xml"
+let sitemap = "https://microbiomehelper.ca/sitemap.xml"
 // Based in https://ben.balter.com/2022/06/30/helpful-404s-for-jekyll-and-github-pages/
 // A: Function definition...
 // Own version of closest from 'fastest-levenshtein')
